8fecedab5c47ba1191f52d4fc2a526dc5f7bbca5
ankitadas100 <ankitadas2308@gmail.com>
2026-03-15 18:33:28 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './Components/Navbar' // Fix 1
 import Home from './Pages/Home'
 import Works from './Pages/Works'
 import Signup1 from './Pages/Signup1'
+import About from './Pages/About'
 function App() {
   
   return (
@@ -16,6 +17,7 @@ function App() {
   <Route path='/' element={<Home />} />
   <Route path='/Works' element={<Works />} />
   <Route path='/signup1' element={<Signup1 />} /> 
+   <Route path='/About' element={<About />} /> 
 </Routes>
     
     </BrowserRouter>
