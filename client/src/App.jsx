06beaeb7e64e1e6f230fbfb203ac5136b6b64300
skrijwan <skrijwan329@gmail.com>
2026-03-15 19:14:38 +0530

    Refactor App component for improved readability and formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,19 @@ import Navbar from './Components/Navbar' // Fix 1
 import Home from './Pages/Home'
 import Works from './Pages/Works'
 import Signup1 from './Pages/Signup1'
-import { ToastContainer,Flip } from 'react-toastify'
+import { ToastContainer, Flip } from 'react-toastify'
 function App() {
 
   return (
     <BrowserRouter>
-    <Navbar />
-    <ToastContainer transition={Flip}/>
-    <Routes>
-  <Route path='/' element={<Home />} />
-  <Route path='/Works' element={<Works />} />
-  <Route path='/signup' element={<Signup1 />} /> 
-</Routes>
-    
+      <Navbar />
+      <ToastContainer transition={Flip} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Works' element={<Works />} />
+        <Route path='/signup' element={<Signup1 />} />
+      </Routes>
+
     </BrowserRouter>
   )
 
