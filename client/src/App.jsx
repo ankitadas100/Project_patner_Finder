2c728552d5950893e511d39a52aa87eba5be77ae
Sk Rijwan <165564445+skrijwan100@users.noreply.github.com>
2026-03-15 19:11:43 +0530

    Refactor App component to include ToastContainer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,19 @@ import Navbar from './Components/Navbar' // Fix 1
 import Home from './Pages/Home'
 import Works from './Pages/Works'
 import Signup1 from './Pages/Signup1'
-import About from './Pages/About'
+import { ToastContainer, Flip } from 'react-toastify'
 function App() {
 
   return (
     <BrowserRouter>
-    <Navbar />
-    <ToastContainer transition={Flip}/>
-    <Routes>
-  <Route path='/' element={<Home />} />
-  <Route path='/Works' element={<Works />} />
-  <Route path='/signup1' element={<Signup1 />} /> 
-   <Route path='/About' element={<About />} /> 
-</Routes>
-    
+      <Navbar />
+      <ToastContainer transition={Flip} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Works' element={<Works />} />
+        <Route path='/signup' element={<Signup1 />} />
+      </Routes>
+
     </BrowserRouter>
   )
 
