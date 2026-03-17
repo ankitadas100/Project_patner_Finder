0e2365e890f64d649c11903dd940e5ac213a4399
skrijwan <skrijwan329@gmail.com>
2026-03-17 16:52:04 +0530

    Update dependencies and implement user registration functionality

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    collagename: {
+        type: String,
+        require: true,
+    },
+    bio:{
+        type:String,
+        require:true
+    },
+    skill: {
+        type: String,
+        require: true,
+    },
+    githublink:{
+        type: String,
+        require: true,
+    },
+    linkedinlink:{
+        type: String,
+        require: true,
+    },
+    protfolio:{
+        types:String,
+    }
+},{ timestamps: true });
+const User = mongoose.model("User", userSchema);
+export default User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express"
 import sendemail from "../middlewares/sendemail.js";
+import User from "../models/User.js";
 const authRouter = express.Router();
 let otp = null;
 
@@ -27,6 +28,32 @@ authRouter.post("/otpverify",async(req,res)=>{
         }
         return res.status(400).json({ "message": "Not Vrifyed", "status": false })
 
+    } catch (error) {
+        console.log(error)
+        return res.status(505).json({ "error": "Internal server error", "status": false })
+    }
+})
+authRouter.post("/register", async(req,res)=>{
+    try {
+        const {fullname,email,collagename,bio,skill,githublink,linkedinlink,protfolio}=req.body;
+        const IsFristUser= User.findOne({email:email});
+        if(!IsFristUser){
+            return res.status(400).json({"message":"Already have an account.",status:false})
+        }
+        const newuser= new User({
+            fullname,
+            email,
+            collagename,
+            bio,
+            skill,
+            githublink,
+            linkedinlink,
+            protfolio
+        })
+       await newuser.save();
+       return res.status(200).json({"message":"Register done",status:true})
+
+        
     } catch (error) {
         console.log(error)
         return res.status(505).json({ "error": "Internal server error", "status": false })
