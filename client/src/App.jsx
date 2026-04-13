19a01129f4408321912a33a1353f559d9c3c5eed
ankitadas100 <ankitadas2308@gmail.com>
2026-04-13 14:19:43 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,37 +2,35 @@ import './App.css'
 import { ToastContainer, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar' // Fix 1
+import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
 import Works from './Pages/Works'
 import Signup1 from './Pages/Signup1'
 import About from './Pages/About'
 import Signup2 from './Pages/Signup2';
 import Login from './Pages/Login';
-<<<<<<< HEAD
+import Footer from './Components/Footer';
+import Requriment from './Pages/Requriment';
+import Requirement2 from "./Pages/Requirement2";
+import Requirement from './Pages/PostRequirement';
+
 import { useEffect, useState } from 'react';
 import { useAuth } from './context/AuthContext';
 import { useUserData } from './context/UserdataContext';
 import LoadingScreen from './Components/LodingScreen';
 import secureLocalStorage from 'react-secure-storage';
-import Requirement from './Pages/PostRequirement';
 
-=======
-import Footer from './Components/Footer';
-import Requriment from './Pages/Requriment';
-import Requirement2 from "./Pages/Requirement2";
->>>>>>> 3ef71b2 (update)
 function App() {
-  const { user } = useAuth()
+  const { user } = useAuth();
   const { setUseralldata } = useUserData();
-  const [Isloginuser, setIsIsloginuser] = useState()
+  const [Isloginuser, setIsIsloginuser] = useState(false);
+
   useEffect(() => {
     const CheckUserLogin = async () => {
       try {
-
-<<<<<<< HEAD
         const token = await user?.getIdToken();
         const localtoken = secureLocalStorage.getItem('auth-token');
+
         if (localtoken) {
           const url = `${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/getuser`;
           const response = await fetch(url, {
@@ -43,10 +41,9 @@ function App() {
             },
           });
           const data = await response.json();
-          setUseralldata(data.userdata)
-           setIsIsloginuser(true)
-         
+          setUseralldata(data.userdata);
         }
+
         if (token) {
           const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/getuser`, {
             headers: {
@@ -54,61 +51,44 @@ function App() {
             }
           });
           const data = await res.json();
-          setUseralldata(data.userdata)
-          setIsIsloginuser(true)
+          setUseralldata(data.userdata);
         }
-      } catch (error) {
-        console.log(error)
-        setIsIsloginuser(true)
-      }
-    }
-    CheckUserLogin();
-  }, [user])
-  if (!Isloginuser)
-    return (
-      <LoadingScreen />
 
-    )
-  if (Isloginuser) {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <ToastContainer transition={Flip} />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Works' element={<Works />} />
-          <Route path='/signup' element={<Signup1 />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Signup2' element={<Signup2 />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/postrequiremen' element={<Requirement />} />
-          
-        </Routes>
-      </BrowserRouter>
-    )
+        setIsIsloginuser(true);
+
+      } catch (error) {
+        console.log(error);
+        setIsIsloginuser(true);
+      }
+    };
+
+    CheckUserLogin();
+  }, [user]);
+
+  if (!Isloginuser) {
+    return <LoadingScreen />;
   }
 
-=======
   return (
     <BrowserRouter>
       <Navbar />
-      <ToastContainer />
       <ToastContainer transition={Flip} />
+
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/Works' element={<Works />} />
+        <Route path='/works' element={<Works />} />
         <Route path='/signup' element={<Signup1 />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Signup2' element={<Signup2 />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signup2' element={<Signup2 />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/Requirement2' element={<Requirement2/>} />
+        <Route path='/Requirement2' element={<Requirement2 />} />
         <Route path='/Requriment' element={<Requriment />} />
+        <Route path='/postrequiremen' element={<Requirement />} />
       </Routes>
-       <Footer/>
+
+      <Footer />
     </BrowserRouter>
-  )
->>>>>>> 3ef71b2 (update)
-
-
+  );
 }
+
 export default App;
